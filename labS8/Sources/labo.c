f5eae0926bb8f146205d18ed79d74635db5f0598
Jessie Leclerc <2330032@bart.ca>
2024-03-28 19:15:29 -0400

    LabS8 final

--- labS8/Sources/labo.c
+++ labS8/Sources/labo.c
@@ -8,6 +8,7 @@
 static long long fib_memo[0x7ffffff];
 
 
+
 /*
 * Programmer l'algorithme de fibonacci.
 * Commencer par faire l'algorithme de base et par la suite ajouter la memoization.
@@ -26,7 +27,7 @@ long long fibonacci(int n) {
 	if (fib_memo[n] == -1) 
 	{
 		fib_memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
-		return fib_memo;
+		return fib_memo[n];
 	}
 	else
 	{
@@ -38,12 +39,16 @@ long long fibonacci(int n) {
 * Creer un Noeud, assigner lui la valeur de data, mettre ses references a NULL et retourner le noeud.
 */
 TreeNode* tree_create_node(void* data) {
-	TreeNode t;
-	t.data = data;
-	t.left = NULL;
-	t.right = NULL;
+	TreeNode* t = (TreeNode*)allocate(sizeof(TreeNode));
+	
+	if (t != NULL) 
+	{
+	t->data = data;
+	t->left = NULL;
+	t->right = NULL;	
+	}
 
-	return &t;
+	return t;
 }
 
 /*
@@ -60,27 +65,72 @@ void tree_add_node(TreeNode* root, TreeNode* node) {
 	{
 		root->right = node;
 	}
-	else if (root->left != NULL && root->right != NULL) 
-	{
-		return 0;
-	}
 }
 
 /*
 * Programmer l'algorithme de depth first search afin de trouver la cle.
 * Pour programmer cet algorithme, vous avez besoins d'une pile/stack, ca definition est fourni.
 */
+
 int dfs(TreeNode* root, void* key)
 {
 	Stack s = stack_init(sizeof(TreeNode));
 
+	stack_push(&s, root);
+
+	int nodes_visited = 0;
+
+	while (s.top != -1) 
+	{
+		TreeNode* curr = (TreeNode*)stack_pop(&s);
+		nodes_visited++;
+
+		if (curr->data == key) 
+		{
+			return nodes_visited;
+		}
+		if (curr->right != NULL) 
+		{
+			stack_push(&s, curr->right);
+		}
+		if (curr->left != NULL) 
+		{
+			stack_push(&s, curr->left);
+		}
+	}
+	return nodes_visited;
 }
 
 /*
 * Programmer l'algorithme de breath first search afin de trouver la cle.
 * Pour programmer cet algorithme, vous avez besoins d'une file/queue, ca definition est fourni.
 */
+
 int bfs(TreeNode* root, void* key)
 {
+	Queue q;
+	queue_init(&q);
+	queue_push(&q, root);
 
+	int nodes_visited = 0;
+
+	while (q.data != NULL) 
+	{
+		TreeNode* curr = (TreeNode*)queue_pop(&q);
+		nodes_visited++;
+
+		if (curr->data == key) 
+		{
+			return nodes_visited;
+		}
+		if (curr->left != NULL) 
+		{
+			queue_push(&q, curr->left);
+		}
+		if (curr->right != NULL)
+		{
+			queue_push(&q, curr->right);
+		}
+	}
+	return nodes_visited;
 }
